f3d67e5f2983f2b24e2353befbd0cc34c01fe031
Kris Camilleri <kris.camilleri@gmail.com>
2020-02-13 18:34:59 +0100

    added purgecss

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 // Inside vue.config.js
+const PurgecssPlugin = require("purgecss-webpack-plugin");
+const glob = require("glob-all");
+const path = require("path");
+
 module.exports = {
   // ...other vue-cli plugin options...
   pwa: {
@@ -9,5 +13,16 @@ module.exports = {
     appleMobileWebAppCapable: "yes",
     appleMobileWebAppStatusBarStyle: "black"
     // configure the workbox plugin
+  },
+  configureWebpack: {
+    plugins: [
+      new PurgecssPlugin({
+        paths: glob.sync([
+          path.join(__dirname, "./public/index.html"),
+          path.join(__dirname, "./**/*.vue"),
+          path.join(__dirname, "./src/**/*.js")
+        ])
+      })
+    ]
   }
 };
